fc982ea22b5d0af809efa112d81b228237276807
Ergest Xheblati <ergest@gmail.com>
2023-04-22 16:48:32 -0400

    fixed JSON

--- models/load_activities/company_offboarded_sdr.sql
+++ models/load_activities/company_offboarded_sdr.sql
@@ -6,6 +6,6 @@
 generate_fake_data (
     activity_name = 'offboarded_sdr',
     has_revenue_impact = false,
-    feature_json_dict = '{"tenure": ["t1", "t2", "t3","t4"]'
+    feature_json_dict = '{"tenure": ["t1", "t2", "t3", "t4"]}'
   )
 -}}
