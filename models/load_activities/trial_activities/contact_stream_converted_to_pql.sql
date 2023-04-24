3f15590b781d3147af498d61e006acb8300e9bd7
Ergest Xheblati <ergest@gmail.com>
2023-04-23 20:20:10 -0400

    moved existing stuff to appropriate folders

---
+++ models/load_activities/trial_activities/contact_stream_converted_to_pql.sql
@@ -0,0 +1,15 @@
+{{-
+    config(materialized = 'table')
+-}}
+
+{{-
+generate_fake_data (
+    activity_name = 'converted_to_pql',
+    has_revenue_impact = false,
+    feature_json_dict = '{"subcsription_type": ["mrr", "arr"],
+                         "segment": ["seg1", "seg2", "seg3", "seg4"],
+                         "plan": ["plan1", "plan2", "plan3"],
+                         "csm": ["csm1", "csm2", "csm3", "csm4", "csm5"],
+                         "mrr_tier": ["tier1", "tier2"]}'
+  )
+-}}
