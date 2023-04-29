baef33a35e80626c55baa4f008d4d3030ea0e5b4
Ergest Xheblati <ergest@gmail.com>
2023-04-29 18:52:36 -0400

    fixed build

--- models/load_activities/support_activities/client_stream_reopened_support_ticket.sql
+++ models/load_activities/support_activities/client_stream_reopened_support_ticket.sql
@@ -9,7 +9,6 @@ generate_fake_data (
     feature_json_dict = '{"segment": ["s1", "s2", "s3", "s4"],
                           "mrr_tier": ["t1", "t2", "t3"],
                           "plan": ["p1", "p2", "p3", "p4"],
-                          "csm": ["csm1", "csm2", "csm3"],
-                          "action" ["action1", "action2", "action3"]}'
+                          "csm": ["csm1", "csm2", "csm3"]}'
   )
 -}}
--- models/load_activities/support_activities/client_stream_updated_support_ticket.sql
+++ models/load_activities/support_activities/client_stream_updated_support_ticket.sql
@@ -10,6 +10,6 @@ generate_fake_data (
                           "mrr_tier": ["t1", "t2", "t3"],
                           "plan": ["p1", "p2", "p3", "p4"],
                           "csm": ["csm1", "csm2", "csm3"],
-                          "action" ["pending customer information", "pending agent information", "details provided"]}'
+                          "action": ["pending customer information", "pending agent information", "details provided"]}'
   )
 -}}
