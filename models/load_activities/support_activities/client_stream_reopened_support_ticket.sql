6cdc650bf3b552023fede1e04a21511d8d356a94
Ergest Xheblati <ergest@gmail.com>
2023-04-29 11:22:39 -0400

    created new activities

---
+++ models/load_activities/support_activities/client_stream_reopened_support_ticket.sql
@@ -0,0 +1,15 @@
+{{-
+    config(materialized = 'table')
+-}}
+
+{{-
+generate_fake_data (
+    activity_name = 'reopened_support_ticket',
+    has_revenue_impact = false,
+    feature_json_dict = '{"segment": ["s1", "s2", "s3", "s4"],
+                          "mrr_tier": ["t1", "t2", "t3"],
+                          "plan": ["p1", "p2", "p3", "p4"],
+                          "csm": ["csm1", "csm2", "csm3"],
+                          "action" ["action1", "action2", "action3"]}'
+  )
+-}}
---
+++ models/load_activities/support_activities/client_stream_updated_support_ticket.sql
@@ -0,0 +1,15 @@
+{{-
+    config(materialized = 'table')
+-}}
+
+{{-
+generate_fake_data (
+    activity_name = 'updated_support_ticket',
+    has_revenue_impact = false,
+    feature_json_dict = '{"segment": ["s1", "s2", "s3", "s4"],
+                          "mrr_tier": ["t1", "t2", "t3"],
+                          "plan": ["p1", "p2", "p3", "p4"],
+                          "csm": ["csm1", "csm2", "csm3"],
+                          "action" ["pending customer information", "pending agent information", "details provided"]}'
+  )
+-}}
