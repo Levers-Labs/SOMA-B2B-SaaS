832a6a2b4af2e938a69d3bad4c9293c3412d7751
Ergest Xheblati <ergest@gmail.com>
2023-05-25 18:55:07 -0400

    fixed whitespace

--- models/load_activities/contract_activities/contact_stream_signed_contract.sql
+++ models/load_activities/contract_activities/contact_stream_signed_contract.sql
@@ -7,7 +7,7 @@ generate_fake_data (
     activity_name = 'signed_contract',
     has_revenue_impact = false,
     feature_json_dict = '{"subcsription_type": ["mrr", "arr"],
-"segment": ["seg1", "seg2", "seg3", "seg4"],
+                         "segment": ["seg1", "seg2", "seg3", "seg4"],
                          "plan": ["plan1", "plan2", "plan3"],
                          "csm": ["csm1", "csm2", "csm3", "csm4", "csm5"],
                          "mrr_tier": ["tier1", "tier2"]}'
