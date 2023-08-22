60a7cf811fa262d1f1c39a1c0a1425b910c6085c
Ergest Xheblati <3359788+ergest@users.noreply.github.com>
2023-08-21 20:19:28 -0400

    removed unnecessary update

--- models/client_activity_stream.sql
+++ models/client_activity_stream.sql
@@ -1,47 +1,36 @@
 {{-
-    config(
-        materialized = 'table',
-        post_hook = "
-                update {{ this }} a
-                set activity_occurrence = dt.activity_occurrence,
-                    activity_repeated_at = dt.activity_repeated_at
-                from (
-                    select
-                        id,
-                        customer_id,
-                        activity,
-                        activity_ts,
-                        row_number() over(partition by customer_id, activity order by activity_ts asc) as activity_occurrence,
-                        lead(activity_ts, 1) over(partition by customer_id, activity order by activity_ts asc) as activity_repeated_at
-                    from 
-                        {{ this }}
-                )dt
-                where 
-                    dt.id = a.id
-                    and dt.customer_id = a.customer_id
-                    and dt.activity = a.activity
-                    and a.activity_ts = dt.activity_ts"
-)
+    config(materialized = 'table')
 -}}
 
--- Returns a list of relations that match schema.prefix%
-{%- set all_tables = dbt_utils.get_relations_by_pattern('main', 'client_stream%') -%}
-{% for table in all_tables %}
+with cte_union_streams as (
+    -- Returns a list of relations that match schema.prefix%
+    {%- set all_tables = dbt_utils.get_relations_by_pattern(target.schema, 'client_stream%') -%}
+    {% for table in all_tables %}
+    select
+        id,
+        entity_id as customer_id,
+        activity,
+        activity_ts,
+        revenue_impact,
+        feature_json,
+    from
+        {{ table }}
+    {%- if not loop.last %}
+    union all
+    {%- endif -%}
+    {% endfor %}
+)
 select
     id,
-    entity_id as customer_id,
+    customer_id,
     activity,
     activity_ts,
     revenue_impact,
     feature_json,
-    cast(null as int) as activity_occurrence,
-    cast(null as timestamp) as activity_repeated_at
-from
-    {{ table }}
-{%- if not loop.last %}
-union all
-{%- endif -%}
-{% endfor %}
+    row_number() over(partition by customer_id, activity order by activity_ts asc) as activity_occurrence,
+    lead(activity_ts, 1) over(partition by customer_id, activity order by activity_ts asc) as activity_repeated_at
+from 
+    cte_union_streams
 
 --depends on: {{ ref('client_stream_decreased_contract') }}
 --depends on: {{ ref('client_stream_incurred_overage') }}
